a13866e27f553ab705ccded4d4a14c46671c1c20
Michele Ford <michelesford@gmail.com>
2015-09-14 23:59:56 -0400

    updated resume files for initial project sumbit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,18 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var name			= "Michele Ford";
-var role 			="Front-End Developer";
-var formattedName	= HTMLheaderName.replace("%data%", name);
-var formattedRole	= HTMLheaderRole.replace("%data%", role);
-
-//var skills			= ["HTML", "CSS", "JavaScript", "jQuery", "PHP"];
-
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-//$("#main").append(skills);
 
 var bio = {
     "name": "Michele Ford",
@@ -21,7 +9,7 @@ var bio = {
     "contacts": {
         "email": "michelesford@gmail.com",
         "github": "badwolfgirl",
-        "location": "Myrtle Beach, SC"
+        "location": "Little River, SC"
     },
     "welcomeMessage": "Welcome to my Resume page!",
     "skills": [
@@ -37,16 +25,15 @@ var bio = {
         "Microsoft Office"
     ],
     "biopic" : "images/me.jpg"//,
-    //"display" : 
 };
 
 var education = {
     "schools": [
         {
-            "name": "Central Conn St Univ",
+            "name": "Central Connecticut State University",
             "location": "New Britain, CT",
-            "degree": "BA",
-            "majors": ["Graphic Design"],
+            "degree": "Bachelor of Arts",
+            "major": "Graphic Design",
             "dates": 2005,
             "url": "http://www.ccsu.edu/"
         }
@@ -55,23 +42,65 @@ var education = {
         {
             "title": "Intro to HTML and CSS",
             "school": "Udacity",
-            "dates": "August 2015",
+            "dates": 2015,
             "url" : "https://www.udacity.com/course/intro-to-html-and-css--ud304"
         },
         {
             "title": "How to Use Git and GitHub",
             "school": "Udacity",
-            "dates": "August 2015",
+            "dates": 2015,
             "url" : "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
         },
         {
             "title": "JavaScript Basics",
             "school": "Udacity",
-            "dates": "September 2015",
+            "dates": 2015,
             "url" : "https://www.udacity.com/course/javascript-basics--ud804"
         }
-    ]//,
-    //display: function
+    ],
+    display: function() {
+        if(education.schools.length > 0){
+
+            $("#education").append(HTMLschoolStart);
+
+            for (school in education.schools){
+                var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+
+                var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+                $(".education-entry:last").append(formattedName + formattedDegree);
+
+                var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+                $(".education-entry:last").append(formattedDates);
+
+                var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+                $(".education-entry:last").append(formattedLocation);
+
+                var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+                $(".education-entry:last").append(formattedMajor);
+
+            }
+        }
+        if(education.onlinecourses.length > 0){
+
+            $("#education").append(HTMLonlineClasses);
+            $("#education").append(HTMLschoolStart);
+
+            for (onlineCourse in education.onlinecourses){
+                var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlinecourses[onlineCourse].title);
+
+                var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlinecourses[onlineCourse].school);
+                $(".education-entry:last").append(formattedTitle + formattedSchool);
+
+                var formattedDates = HTMLonlineDates.replace("%data%", education.onlinecourses[onlineCourse].dates);
+                $(".education-entry:last").append(formattedDates);
+
+                var formattedUrl = HTMLonlineURL.replace("%data%", education.onlinecourses[onlineCourse].url);
+                $(".education-entry:last").append(formattedUrl);
+
+            }
+        }
+
+    }
 };
 
 var work = {
@@ -94,41 +123,84 @@ var work = {
             "description": "Working with a 250 Unit Franchise System, main responsibilities included Graphic Design, Web Development and some hosting management. Using a CodeIgniter Framework, developed a Reporting system for the Franchise Areas to input their monthly sales, monthly meeting information, yearly tax information and checkoff lists. Designed various print materials for individuals in the system with tight deadlines, and developed the current web site with the requirement of a WordPress platform. Also helped to edit the system newsletter and edit videos."
         },
         {
-            "employer": "Web Programmer/Graphic Designer",
-            "title": "Creative Director",
+            "employer": "Rokap Inc. d/b/a Kurbot LLC.",
+            "title": "Web Programmer/Graphic Designer",
             "location": "Wallingford, CT",
             "dates": "May 2008 - January 2013",
             "description": "Daily responsibilities consisted of web site development and hosting management for various clients. Including basic Informational sites, customized Content Management sites, E-Commerce sites, and some custom web application development. My minor responsibilities included troubleshooting client issues over the phone to fix problems such as e-mail connections. Tasked also with delegating work to remote employee."
         }
     ]//,
-    //display: function
 };
 
 var projects = {
     "projects": [
         {
-            "title": "Project 0",
-            "dates": "August 2015",
-            "description": "Intro Project",
-            "images": "na"
-        },
-        {
-            "title": "Project 1",
-            "dates": "August 2015",
-            "description": "Portfolio",
-            "images": "na"
+            "title": "Project 2",
+            "dates": 2015,
+            "description": "Javascript Basics Resume Builder",
+            "images": ["images/fry.jpg"]
         }
-    ]//,
-    //display: function
+    ],
+    display: function() {
+        for (project in projects.projects){
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+            $(".project-entry:last").append(formattedTitle);
+
+            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+            $(".project-entry:last").append(formattedDates);
+
+            var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+            $(".project-entry:last").append(formattedDescription);
+
+            if(projects.projects[project].images.length > 0){
+                for(var image in projects.projects[project].images) {
+                    var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                    $(".project-entry:last").append(formattedImage);
+
+                }
+            }
+
+        }
+    }
 };
 
-if(bio.skills.length > 0){
+if(bio !== ''){
 
-    $("#header").append(HTMLskillsStart);
+    var formattedRole   = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
 
-    for(skill in bio.skills){
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-        $("#skills").append(formattedSkill);
+    var formattedName   = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(formattedName);
+
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts").append(formattedEmail);
+    $("#footerContacts").append(formattedEmail);
+
+    var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts").append(formattedGitHub);
+    $("#footerContacts").append(formattedGitHub);
+
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedLocation);
+    $("#footerContacts").append(formattedLocation);
+
+    var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(formattedBiopic);
+
+    var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedWelcome);
+
+    if(bio.skills.length > 0){
+
+        $("#header").append(HTMLskillsStart);
+
+        for(skill in bio.skills){
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+            $("#skills").append(formattedSkill);
+        }
+
     }
 
 }
@@ -153,7 +225,17 @@ function displayWork() {
      $(".work-entry:last").append(formattedDescription);
     }
 }
-displayWork();
+// if there are jobs in work then display work function
+if(work.jobs.length > 0){
+    displayWork();
+}
+// if there are projects in projects then display function
+if(projects.projects.length > 0){
+    projects.display();
+}
+if(education.schools.length > 0){
+    education.display();
+}
 
 // LOCATION ARRAY FUNCTION
 function locationizer(work_obj){
@@ -166,32 +248,6 @@ function locationizer(work_obj){
     // return the final array
     return locationArray;
 }
-
-//project functions
-projects.display = function() {
-    for (project in projects.projects){
-        $("#projects").append(HTMLprojectStart);
-
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-        $(".project-entry:last").append(formattedTitle);
-
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-        $(".project-entry:last").append(formattedDates);
-
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-        $(".project-entry:last").append(formattedDescription);
-
-        if(projects.projects[project].images.length > 0){
-            for(image in projects.projects[project].images) {
-                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-                $(".project-entry:last").append(formattedImage);
-
-            }
-        }
-
-    }
-}
-projects.display();
 
 // function to change name (does not work)
 function inName(name) {
